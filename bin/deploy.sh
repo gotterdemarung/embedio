588b61dcb77294fa2f8cc09fbf465c614ddc6135
gotterdemarung <gotterdemarung@gmail.com>
2013-10-26 19:27:00 +0300

    deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,3 +4,9 @@ PATH_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${PATH_SCRIPT}"/../"
 
 composer update
+
+# Frontend vendor folder
+mkdir htdocs/vendor
+
+# Twitter bootstrap
+ln -fs vendor/twitter/boostrap/dist htdocs/vendor/bootstrap
